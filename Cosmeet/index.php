efcea80105748f3bc69b484ca1e4195dd2f59450
Elliot Lartigaud <elliot.lartigaud@etu.univ-amu.fr>
2023-11-05 00:50:24 +0100

    index

--- Cosmeet/index.php
+++ Cosmeet/index.php
@@ -2,21 +2,7 @@
 // Ce fichier est le point d'entrée de votre application
 
 require 'Noyau/ChargementAuto.php';
-/*
- url pour notre premier test MVC Hello World,
- nous n'avons pas d'action précisée on visera celle par défaut
- /index.php?ctrl=helloworld
- /helloworld
- /controleur/nom_action/whatever/whatever2/
 
-*/
-/*
-    $S_controleur = isset($_GET['ctrl']) ? $_GET['ctrl'] : null;
-    $S_action = isset($_GET['action']) ? $_GET['action'] : null;
-
-    Vue::ouvrirTampon(); //  /Noyau/Vue.php : on ouvre le tampon d'affichage, les contrôleurs qui appellent des vues les mettront dedans
-    $O_controleur = new Controleur($S_controleur, $S_action);
-*/
 session_start();
 $S_urlADecortiquer = isset($_GET['url']) ? $_GET['url'] : null;
 if (!$S_urlADecortiquer) {
@@ -26,13 +12,10 @@ $A_postParams = isset($_POST) ? $_POST : null;
 
 Vue::ouvrirTampon(); // on ouvre le tampon d'affichage, les contrôleurs qui appellent des vues les mettront dedans
 
-try
-{
+try {
     $O_controleur = new Controleur($S_urlADecortiquer, $A_postParams);
     $O_controleur->executer();
-}
-catch (ControleurException $O_exception)
-{
+} catch (ControleurException $O_exception) {
     echo ('Une erreur s\'est produite : ' . $O_exception->getMessage());
 }
 
