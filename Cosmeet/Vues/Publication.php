e3d9ed64deba8b3e4dec0f3369693abc4ad9e769
Elliot Lartigaud <elliot.lartigaud@etu.univ-amu.fr>
2023-11-05 00:47:16 +0100

    Publication indenté et sémantique

--- Cosmeet/Vues/Publication.php
+++ Cosmeet/Vues/Publication.php
@@ -9,14 +9,19 @@
 
 <body>
     <div class="publication">
+
         <h2><?php echo $publication['titre']; ?></h2>
         <p><?php echo $publication['message']; ?></p>
+
         <?php
         $email = $publication['auteur'];
         $controleur = new ControleurPublication();
         $pseudoPost = $controleur->getPseudo($email);
         ?>
-        <p>Publié le <?php echo $publication['date_publication']; ?> par <a class="text-style" href="index.php?url=Compte&email=<?php echo $publication['auteur']; ?>"><?php echo $pseudoPost; ?></a></p>
+
+        <p> Publié le <?php echo $publication['date_publication']; ?> par
+            <a class="text-lien" href="index.php?url=Compte&email=<?php echo $publication['auteur']; ?>"><?php echo $pseudoPost; ?></a>
+        </p>
         </p>
 
         <?php if ($_SESSION['utilisateur']['email'] == $publication['auteur']) : ?>
@@ -25,26 +30,36 @@
 
         <button id="add-comment-button" onclick="window.location.href='../Cosmeet/index.php?url=Commentaire&id_publication=<?php echo $publication['id_publication']; ?>'">Ajouter un commentaire</button>
         <button class="toggle-comments-button">Commentaires</button>
+
         <div class="commentaires" style="display: none;">
+
             <?php foreach ($commentaires as $commentaire) : ?>
+
                 <?php
                 $email = $commentaire['auteur'];
                 $controleur = new ControleurPublication();
                 $pseudoCommentaire = $controleur->getPseudo($email);
                 ?>
+
                 <?php if ($commentaire['id_publication'] == $publication['id_publication']) : ?>
-                    <p><?php echo $commentaire['commentaire']; ?> | <a class="text-style" href="index.php?url=Compte&email=<?php echo $commentaire['auteur']; ?>"><?php echo $pseudoCommentaire; ?></a>| <?php echo $commentaire['date_commentaire']; ?>
+                    <p><?php echo $commentaire['commentaire']; ?> |
+                    <a class="text-lien" href="index.php?url=Compte&email=<?php echo $commentaire['auteur']; ?>"><?php echo $pseudoCommentaire; ?></a> |
+                    <?php echo $commentaire['date_commentaire']; ?>
                         <?php if ($_SESSION['utilisateur']['email'] == $commentaire['auteur']) : ?>
                             <button onclick="window.location.href='index.php?url=ModifierCom&id_commentaire=<?php echo $commentaire['id_commentaire']; ?>'">Modifier</button>
                         <?php endif; ?>
                     </p>
                 <?php endif; ?>
+
             <?php endforeach; ?>
+
         </div>
     </div>
 </body>
 
 </html>
+
 <style>
-    @import url("./CSS/Accueil.css");
+    @import url("./CSS/MenuCoulissant.css");
+    @import url("./CSS/Publication.css");
 </style>
