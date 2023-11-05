2cae5b6758bc80b9cce51483b6abca8a5d2723f4
Elliot Lartigaud <elliot.lartigaud@etu.univ-amu.fr>
2023-11-05 12:53:53 +0100

    Commentaire semantique et indentation

--- Cosmeet/Vues/Commentaire.php
+++ Cosmeet/Vues/Commentaire.php
@@ -6,6 +6,7 @@ if (isset($A_vue['reussite'])) {
     $condition = $A_vue['erreur'];
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -14,23 +15,32 @@ if (isset($A_vue['reussite'])) {
 </head>
 
 <body>
-    <div id="formulaire">
-        <form method="POST" action="../Cosmeet/index.php?url=Commentaire/addCommentaire" enctype="multipart/form-data">
-            <div id="Onglets">
-                <h3><a id="Retour" href="../Cosmeet/index.php?url=Accueil">RETOUR</a></h3>
-            </div>
-            <textarea name="commentaire" placeholder="COMMENTAIRE" required></textarea>
-            <button class="boutonLog" name="boutonLog" type="submit">Publier</button>
-            <input type="hidden" name="id_publication" value="<?php echo $_GET['id_publication']; ?>">
+    <div class="Commentaire">
+        <div id="formulaire">
 
-            <h1 style="color: red;">
-                <?php echo $condition ?>
-            </h1>
-        </form>
+            <form method="POST" action="../Cosmeet/index.php?url=Commentaire/addCommentaire" enctype="multipart/form-data">
+
+                <div id="Onglets">
+                    <h3><a id="Retour" href="../Cosmeet/index.php?url=Accueil">RETOUR</a></h3>
+                </div>
+
+                <textarea name="commentaire" placeholder="COMMENTAIRE" required></textarea>
+                <button class="boutonLog" name="boutonLog" type="submit">Publier</button>
+                <input type="hidden" name="id_publication" value="<?php echo $_GET['id_publication']; ?>">
+
+                <h1 style="color: red;">
+                    <?php echo $condition ?>
+                </h1>
+
+            </form>
+
+        </div>
     </div>
 </body>
 
 </html>
+
 <style>
-    @import url("./CSS/Accueil.css");
+    @import url("./CSS/Commentaire.css");
+    @import url("./CSS/MenuCoulissant.css");
 </style>
