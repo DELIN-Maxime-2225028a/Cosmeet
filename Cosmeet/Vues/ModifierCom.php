2f27f8888f0db98402711a51f0cc5a2e7c79bc68
Elliot Lartigaud <elliot.lartigaud@etu.univ-amu.fr>
2023-11-05 01:59:46 +0100

    modifier com indentataion semantique

--- Cosmeet/Vues/ModifierCom.php
+++ Cosmeet/Vues/ModifierCom.php
@@ -6,6 +6,7 @@ if (isset($A_vue['reussite'])) {
     $condition = $A_vue['erreur'];
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -14,27 +15,35 @@ if (isset($A_vue['reussite'])) {
 </head>
 
 <body>
-    <div id="formulaire">
-        <form method="POST" action="../Cosmeet/index.php?url=ModifierCom/modifierCom" enctype="multipart/form-data">
-            <div id="Onglets">
-                <h3><a id="Retour" href="../Cosmeet/index.php?url=Accueil">RETOUR</a></h3>
-            </div>
-            <h1>Modifier Commentaire</h1>
-            <textarea name="commentaire" placeholder="COMMENTAIRE" required><?php echo $commentaire['commentaire']; ?></textarea>
-            <input type="hidden" name="id_commentaire" value="<?= $commentaire['id_commentaire'] ?>">
-            <button type="submit">Modifier</button>
-        </form>
-        <form method="POST" action="../Cosmeet/index.php?url=ModifierCom/suprimerCom" enctype="multipart/form-data">
-            <input type="hidden" name="id_commentaire" value="<?= $commentaire['id_commentaire'] ?>">
-            <button type="submit">Supprimer</button>
-        </form>
-        <h1 style="color: red;">
-            <?php echo $condition ?>
-        </h1>
+    <div class="ModifierCom">
+        <div id="formulaire">
+
+            <form method="POST" action="../Cosmeet/index.php?url=ModifierCom/modifierCom" enctype="multipart/form-data">
+                <div id="Onglets">
+                    <h3><a id="Retour" href="../Cosmeet/index.php?url=Accueil">RETOUR</a></h3>
+                </div>
+                <h1>Modifier Commentaire</h1>
+                <textarea name="commentaire" placeholder="COMMENTAIRE" required><?php echo $commentaire['commentaire']; ?></textarea>
+                <input type="hidden" name="id_commentaire" value="<?= $commentaire['id_commentaire'] ?>">
+                <button type="submit">Modifier</button>
+            </form>
+
+            <form method="POST" action="../Cosmeet/index.php?url=ModifierCom/suprimerCom" enctype="multipart/form-data">
+                <input type="hidden" name="id_commentaire" value="<?= $commentaire['id_commentaire'] ?>">
+                <button type="submit">Supprimer</button>
+            </form>
+
+            <h1 style="color: red;">
+                <?php echo $condition ?>
+            </h1>
+
+        </div>
     </div>
 </body>
 
 </html>
+
 <style>
-    @import url("./CSS/Accueil.css");
+    @import url("./CSS/ModifierCom.css");
+    @import url("./CSS/MenuCoulissant.css");
 </style>
