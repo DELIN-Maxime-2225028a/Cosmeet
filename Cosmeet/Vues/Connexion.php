571a2c939b072f7d9866237f950471bbfd0e2ccd
Elliot Lartigaud <elliot.lartigaud@etu.univ-amu.fr>
2023-11-05 12:46:49 +0100

    Connexion indentation semantique

--- Cosmeet/Vues/Connexion.php
+++ Cosmeet/Vues/Connexion.php
@@ -6,6 +6,7 @@ if (isset($A_vue['reussite'])) {
     $condition = $A_vue['erreur'];
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -14,28 +15,32 @@ if (isset($A_vue['reussite'])) {
 </head>
 
 <body>
-    <div id="formulaire">
-        <form method="POST" action="../Cosmeet/index.php?url=Connexion/verifierUtilisateur" enctype="multipart/form-data">
+    <div class="Connexion">
+        <div id="formulaire">
 
-            <div id="Onglets">
-                <h3><a id="Connexion">SE CONNECTER</a> <a id="Inscription" href="../Cosmeet/index.php?url=Inscription"> S'INSCRIRE</a></h3>
-            </div>
-            <input type="text" name="pseudo" placeholder="PSEUDO" required>
-            <input type="password" name="mdp1" placeholder="MOT DE PASSE" required>
+            <form method="POST" action="../Cosmeet/index.php?url=Connexion/verifierUtilisateur" enctype="multipart/form-data">
 
-            <button class="boutonLog" name="boutonLog" type="submit">Valider</button>
+                <div id="Onglets">
+                    <h3><a id="Connexion">SE CONNECTER</a> <a id="Inscription" href="../Cosmeet/index.php?url=Inscription"> S'INSCRIRE</a></h3>
+                </div>
 
-            <h1 style="color: red;">
-                <?php echo $condition ?>
-            </h1>
+                <input type="text" name="pseudo" placeholder="PSEUDO" required>
+                <input type="password" name="mdp1" placeholder="MOT DE PASSE" required>
+                <button class="boutonLog" name="boutonLog" type="submit">Valider</button>
 
-        </form>
+                <h1 style="color: red;">
+                    <?php echo $condition ?>
+                </h1>
+
+            </form>
+
+        </div>
     </div>
 </body>
 
 </html>
 
 <style>
-    @import url("./CSS/Accueil.css");
     @import url("./CSS/Connexion.css");
+    @import url("./CSS/MenuCoulissant.css");
 </style>
