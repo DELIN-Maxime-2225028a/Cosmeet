797c022056d158b9bd6d3069405f02175c62a18f
Elliot Lartigaud <elliot.lartigaud@etu.univ-amu.fr>
2023-11-05 12:49:40 +0100

    Compte semantique indentation

--- Cosmeet/Vues/Compte.php
+++ Cosmeet/Vues/Compte.php
@@ -2,21 +2,25 @@
 <html>
 
 <head>
+    <title>Cosmeet</title>
 </head>
 
 <body>
-    <div class="categorie">
+    <div class="Compte">
         <div class="user-info">
+
             <p>Nom d'utilisateur : <?php echo $A_vue['pseudo'] ?></p>
             <p>Adresse email : <?php echo $A_vue['email'] ?></p>
             <p>Date inscription : <?php echo $A_vue['DateInscription'] ?></p>
             <p>Dernier connection : <?php echo $A_vue['DateConnexion'] ?></p>
+
         </div>
     </div>    
 </body>
 
 </html>
+
 <style>
-    @import url("./CSS/Accueil.css");
-    @import url("./CSS/Utilisateur.css");
+    @import url("./CSS/Compte.css");
+    @import url("./CSS/MenuCoulissant.css");
 </style>
