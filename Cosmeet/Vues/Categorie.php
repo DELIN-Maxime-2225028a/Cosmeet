0985ec7497b41ff7152665e3086be9c06c032da3
Elliot Lartigaud <elliot.lartigaud@etu.univ-amu.fr>
2023-11-05 12:57:19 +0100

    categorie indentation semantique

--- Cosmeet/Vues/Categorie.php
+++ Cosmeet/Vues/Categorie.php
@@ -7,12 +7,15 @@
 
 <body>
     <div class="categorie">
+
         <h3><?php echo $categorie['nom_categorie']; ?></h3>
         <p><?php echo $categorie['description_categorie']; ?></p>
+
     </div>
 </body>
 
 </html>
+
 <style>
-    @import url("./CSS/Accueil.css");
+    @import url("./CSS/Categorie.css");
 </style>
